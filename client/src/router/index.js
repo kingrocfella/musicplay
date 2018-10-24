4407be957716cdea07e3d8fddf5a94813321998b
Kingsley Abia <Kingsley.Abia@vfdgroupltd.com>
2018-10-24 10:28:41 +0100

    Fixed the register form bug

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import register from '@/components/register'
 import login from '@/components/login'
 import songs from '@/components/songs'
@@ -11,11 +10,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/register',
       component: register
--- server/src/routes.js
+++ server/src/routes.js
@@ -3,7 +3,7 @@ const authControllerPolicy = require('./policies/authControllerPolicy')
 const songsController = require('./controllers/songsController');
 
 module.exports = (app) => {
-    app.post('/register', authControllerPolicy.register, authController.register),
+    app.post('/register', authController.register),
     app.post('/login', authController.login)
     app.get('/songs', songsController.index)
     app.get('/songs/:songId', songsController.show)
